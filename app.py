4a702daaabf692afee3d3920c23e69aa53fb1f41
alikhanp <86925503+alikhanp@users.noreply.github.com>
2024-03-06 14:01:47 -0500

    Update app.py
    Edit output

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def run(): #Function that runs when button pressed.
       {"role": "user", "content": "Write an answer to the question " + '"' + search_string + '"' + " from using the passages above"}
     ]
   )
-  output = completion.choices[0].message.replace("\n", "\n")
+  output = completion.choices[0].message
   st.info(output) #Display ChatGPT's response
     
   st.subheader("References") #Display all 10 text references with book name and relevance
